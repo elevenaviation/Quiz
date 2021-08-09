2d1c72cd9ac18a1477cddceb747ed284a24ffe03
noobieprogramer <88634339+noobieprogramer@users.noreply.github.com>
2021-08-09 20:59:55 +0100

    Quiz.py
    The Quiz itself

---
+++ Quiz.py
@@ -0,0 +1,116 @@
+print ("Today we will start the installation fase.")
+
+answer = input ("Starting the installation. Do you confirm?")
+
+if answer == " start": 
+    print ("OK! Starting installation.")
+
+else: 
+    quit()
+
+print ("0%...................100%") 
+"/n"
+print("Installing questions...")
+"/n"
+print("Installing question 1/6...")
+"/n"
+print("Installing question 6/6...")
+"/n"
+print ("Installation over. You will be redirected to the setup page.")
+
+answer = input ("Do you confirm the setup process starting?")
+
+if answer == " start":
+    print ("Redirecting...")
+
+else:
+    quit()
+
+answer = input ("To confirm the terms of services, say accept.")
+
+if answer == " accept":
+    print("Redirecting......")
+
+else:
+    quit()
+
+print("Nice!")
+
+name = input ("Insert your name here.")
+if name == name:
+    print()
+
+print("Lets start!")
+
+answer = input ("What does .py mean?")
+
+if answer == " python file":
+    print()
+
+else:
+    print("Incorrect!")
+    
+    
+answer = input ("What is JS?")
+
+if answer == " JavaScript":
+    print()
+    print("Congrats you got the 2nd one right!")
+    print()
+
+else:
+    print()
+    print("Incorrect.")
+    print()
+    
+ 
+answer = input ("What is the meaning for C# (how do you say it)?")
+
+if answer == " C Sharp":
+    print("You are better than 29 percent of players. This will get harder and harder.")
+
+else:
+    print("Incorrect, sorry!")
+    
+
+answer = input ("What is the new version of python?")
+
+if answer == " 3.6":
+    print("Only 2 left!")
+
+else:
+    print ("Incorrect, sorry.")
+
+answer = input ("What is the new version of the coding software HTML?")
+
+if answer == " 5":
+    print("Amazing")
+    print("Question 5/6")
+
+else:
+    print ("Incorrect, sorry!")
+    
+
+answer = input ("Last question, how many people exist in the world?")
+
+if answer == " 7.8 bilion":
+    print("WOW! You got all six!")
+
+else:
+    print ("Incorrect, sorry.")
+
+
+answer = input ("Here are the credits. Say start to start the credits.")
+
+if answer == " start":
+    print("Developer: Eleven Aviation#1402")
+    "/n"
+    print("Question maker: Eleven Aviation#1402")
+    "/n"
+    print("Copyright owner: Eleven Aviation#1402")
+    "/n"
+    print("Eleven Aviation#1402's channel: www.youtube.com/c/IsabelRibeiro2021")
+    "/n"
+    print("Eleven Aviation #1402 is not plausible for any damages to your computer.")
+    "/n"
+    print("For support message Eleven Aviation#1402 on discord, or shoot an email to fakeeurovisionsongcontest@gmail.com")
